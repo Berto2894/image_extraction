3b77421ec7a270749f23e5376623095ebb14c918
Berto2894 <136987795+Berto2894@users.noreply.github.com>
2024-01-28 21:22:50 +0700

    Add files via upload

---
+++ image_extraction.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PIL import Image
+import pytesseract
+
+# Path ke binary Tesseract (contoh di Windows)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\uSeR\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+def extract_text_from_image(image):
+    # Ekstraksi teks menggunakan Tesseract OCR
+    text = pytesseract.image_to_string(image)
+    return text
+
+def main():
+    st.title("Ekstraksi Teks dari Gambar")
+
+    # File Uploader untuk mengunggah gambar
+    uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Tampilkan gambar yang diunggah
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Gambar yang Diunggah", use_column_width=True)
+
+        # Tombol untuk ekstraksi teks
+        if st.button("Ekstrak Teks"):
+            # Ekstraksi teks dari gambar
+            extracted_text = extract_text_from_image(image)
+            
+            # Tampilkan hasil ekstraksi
+            st.subheader("Hasil Ekstraksi Teks:")
+            st.text(extracted_text)
+
+if __name__ == "__main__":
+    main()
